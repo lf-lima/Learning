c3a234a9fa2094cf892bb1c6d4381252d3b43a44
lflima4 <lflima4@stefanini.com>
2020-06-08 11:42:54 -0300

    adjusts

--- JWT-TS-NODE/src/app/middlewares/authentication.ts
+++ JWT-TS-NODE/src/app/middlewares/authentication.ts
@@ -11,39 +11,43 @@ interface ITokenPayload {
 }
 
 export default async (req: Request, res: Response, next: NextFunction): Promise<void | Response> => {
-  const authHeader = req.headers.authorization
+  try {
+    const authHeader = req.headers.authorization
 
-  if (!authHeader) {
-    return res.status(401).json({ error: 'Token not provided' })
+    if (!authHeader) {
+      return res.status(401).json({ error: 'Token not provided' })
+    }
+
+    const parts = authHeader?.split(' ') as string[]
+
+    if (parts.length !== 2) {
+      return res.status(401).json({ error: 'Token error' })
+    }
+
+    const [scheme, token] = parts
+
+    if (scheme !== 'Bearer') {
+      return res.status(401).json({ error: 'Token malformatted' })
+    }
+
+    const tokenParts = token.split('.') as string[]
+
+    if (tokenParts.length !== 3) {
+      return res.status(401).json({ error: 'Token malformatted' })
+    }
+
+    const payload = jwt.verify(token, authConfig.secret) as ITokenPayload
+
+    const user = await userRepository.findById(payload.id, { returnPassword: false }) || new User()
+
+    if (user.isEmpty()) {
+      return res.status(400).json({ error: 'User not exists' })
+    }
+
+    req.user = user
+
+    return next()
+  } catch (error) {
+    return res.status(500).json({ errror: 'Server Internal Error' })
   }
-
-  const parts = authHeader?.split(' ') as string[]
-
-  if (parts.length !== 2) {
-    return res.status(401).json({ error: 'Token error' })
-  }
-
-  const [scheme, token] = parts
-
-  if (scheme !== 'Bearer') {
-    return res.status(401).json({ error: 'Token malformatted' })
-  }
-
-  const tokenParts = token.split('.') as string[]
-
-  if (tokenParts.length !== 3) {
-    return res.status(401).json({ error: 'Token malformatted' })
-  }
-
-  const payload = jwt.verify(token, authConfig.secret) as ITokenPayload
-
-  const user = await userRepository.findById(payload.id, { returnPassword: false }) || new User()
-
-  if (user.isEmpty()) {
-    return res.status(400).json({ error: 'User not exists' })
-  }
-
-  req.user = user
-
-  return next()
 }
