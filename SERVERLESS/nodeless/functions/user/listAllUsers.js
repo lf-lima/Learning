2999eaa0512f9da4670ff8e71d76e01be7bb6172
lflima4 <lflima4@stefanini.com>
2020-08-25 15:59:13 -0300

    nodeless offilne

---
+++ SERVERLESS/nodeless/functions/user/listAllUsers.js
@@ -0,0 +1,24 @@
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ 
+      users: [
+        {
+          id: 1,
+          email: 'luiz@g.com',
+          password: 'luiz123'
+        },
+        {
+          id: 2,
+          email: 'luiz2@g.com',
+          password: 'luiz123'
+        },
+        {
+          id: 3,
+          email: 'luiz3@g.com',
+          password: 'luiz123'
+        },
+      ] 
+    })
+  }
+}
